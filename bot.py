a5bd1faae9c47e0f5690dbda49595d0e3fa3c0c2
BlockChang <stevebendict@gmail.com>
2025-04-09 14:55:48 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ print(">>> DEBUG: Loading environment variables...")
 # ✅ Hardcoded config
 BOT_TOKEN = "7728785789:AAEK7O-NBdl4XD1DhtpBM_oeGb48Ptl7ZpA"
 ADMIN_IDS = [6043250029, 7664906760]
-TARGET_CHANNEL_PUBLIC = "-1002500055359"     # Public Channel
+TARGET_CHANNEL_PUBLIC = "-1002642246708"     # Public Channel
 TARGET_CHANNEL_PRIVATE = "-1002579354695"    # Paid Private Channel
 
 # ✅ Optional debug print
